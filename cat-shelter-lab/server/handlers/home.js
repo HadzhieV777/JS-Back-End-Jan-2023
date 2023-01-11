01effd2466aa1b38da3e9f03f501867631f60ea1
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-11 16:12:02 +0200

    Created req handler

--- cat-shelter-lab/server/handlers/home.js
+++ cat-shelter-lab/server/handlers/home.js
@@ -1,7 +1,32 @@
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 // Objects
-const cats = require('../data/cats.json');
-const breeds = require('../data/breeds.json');
+// const cats = require("../data/cats.json");
+// const breeds = require("../data/breeds.json");
+
+module.exports = (req, res) => {
+  const pathname = url.parse(req.url).pathname;
+
+  if (pathname === "/" && req.method === "GET") {
+    let filePath = path.normalize(
+      path.join(__dirname, "../views/index.html")
+    )
+
+    res.writeHead(200, { "content-type": "text/html" });
+
+    fs.readFile(filePath, null, function (error, data) {
+      if (error) {
+        res.writeHead(404);
+        res.write("Whoops! File not found!");
+      } else {
+        res.write(data);
+      }
+      res.end();
+    });
+    
+  } else {
+    return true;
+  }
+};
