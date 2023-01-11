541085a1163543af437c8d321fd68c850fdc0952
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-11 15:25:52 +0200

    Added handlers dir

---
+++ cat-shelter-lab/server/handlers/home.js
@@ -0,0 +1,7 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+// Objects
+const cats = require('../data/cats.json');
+const breeds = require('../data/breeds.json');
