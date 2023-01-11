e2ab832653a3795dd00b5807e9d1cdcf39ba6f03
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-11 16:12:24 +0200

    Handling homepage req

--- cat-shelter-lab/server/index.js
+++ cat-shelter-lab/server/index.js
@@ -2,10 +2,17 @@ const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Handlers
+const handlers = require('./handlers/index')
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home page</h1>");
-    res.end()
+   
+    for (let handler of handlers) {
+        if(!handler(req, res)) {
+            break;
+        }
+    }
+
 })
 
 server.listen(port, hostname, () => {
