1ff4d2747dfa2075b6abd1d9007cf406c8ecfa23
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-12 11:58:54 +0200

    Edited eventBus.js

--- 02.Streams-and-Utilities/pub-sub-demo/eventBus.js
+++ 02.Streams-and-Utilities/pub-sub-demo/eventBus.js
@@ -3,3 +3,14 @@ const events = {};
 function subscribe(eventName, callback) {
     
 }
+
+function publish(eventName, data) {
+
+}
+
+const eventBus = {
+    subscribe,
+    publish,
+}
+
+module.exports = eventBus
