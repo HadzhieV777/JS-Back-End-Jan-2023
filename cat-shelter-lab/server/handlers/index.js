17f96ee7eed124f786946f364ec7e9d5657efaa2
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-13 12:45:44 +0200

    Edited index.js

--- cat-shelter-lab/server/handlers/index.js
+++ cat-shelter-lab/server/handlers/index.js
@@ -1,4 +1,5 @@
 const handleHome = require('./home');
 const staticFiles = require('./static-file');
+const cat = require('./cat')
 
-module.exports = [handleHome, staticFiles]
+module.exports = [handleHome, staticFiles, cat]
