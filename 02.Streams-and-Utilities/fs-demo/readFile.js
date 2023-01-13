8c39a1641921af232726048c895c3f192e290aa6
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-13 09:49:31 +0200

    Edited readFile.js

--- 02.Streams-and-Utilities/fs-demo/readFile.js
+++ 02.Streams-and-Utilities/fs-demo/readFile.js
@@ -1,6 +1,30 @@
-const fs = require('fs')
+const fs = require("fs");
+const fsp = require("fs/promises");
+
+const path = require("path");
+const filePath = path.resolve(__dirname, "./data.txt");
 
 // Sync read from file
-const text = fs.readFileSync('./data.txt', {encoding: 'utf-8'});
-console.log('Read from file');
-onslotchange.log(text)
+const text = fs.readFileSync(filePath, {
+  encoding: "utf-8",
+});
+console.log("Read from file");
+onslotchange.log(text);
+
+// Async > Sync always!
+
+// Async read from file
+const asyncText = fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
+  if (err) {
+    return;
+  }
+
+  console.log(data);
+});
+console.log("Async read from file");
+
+//  Async with promisses
+fsp.readFile(filePath, { encoding: "utf-8" }).then((result) => {
+  console.log(result);
+});
+console.log("Async with promisses read from file");
