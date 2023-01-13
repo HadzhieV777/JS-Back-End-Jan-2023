838683b25f5c788546000ec706c1c2cb63b999ec
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-13 09:52:09 +0200

    Edited home.js

--- cat-shelter-lab/server/handlers/home.js
+++ cat-shelter-lab/server/handlers/home.js
@@ -3,20 +3,18 @@ const fs = require("fs");
 const path = require("path");
 
 // Objects
-// const cats = require("../data/cats.json");
-// const breeds = require("../data/breeds.json");
+const cats = require("../data/cats");
+const breeds = require("../data/breeds");
 
 module.exports = (req, res) => {
   const pathname = url.parse(req.url).pathname;
 
   if (pathname === "/" && req.method === "GET") {
-    let filePath = path.normalize(
-      path.join(__dirname, "../views/index.html")
-    )
+    let filePath = path.normalize(path.join(__dirname, "../views/index.html"));
 
-    res.writeHead(200, { "content-type": "text/html" });
+    res.writeHead(200, { "Content-Type": "text/html" });
 
-    fs.readFile(filePath, null, function (error, data) {
+    fs.readFile(filePath, (error, data) => {
       if (error) {
         res.writeHead(404);
         res.write("Whoops! File not found!");
@@ -25,7 +23,6 @@ module.exports = (req, res) => {
       }
       res.end();
     });
-    
   } else {
     return true;
   }
