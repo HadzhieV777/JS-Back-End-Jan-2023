0de18faa2a4fb6398dedbb0b47799fdbaaf69245
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-13 09:50:37 +0200

    Edited readableStream.js

--- 02.Streams-and-Utilities/streams-demo/readableStream.js
+++ 02.Streams-and-Utilities/streams-demo/readableStream.js
@@ -1,3 +1,12 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const readStream = fs.createReadStream('./data.txt');
+const readStream = fs.createReadStream("./data.txt");
+
+readStream.on("data", () => {
+  console.log("###### New Chunk ######");
+  console.log(chunk);
+});
+
+readStream.on('close', () => {
+    console.log('Stream closed')
+})
