2d78bdca4ee58efef7c3f3ae8a5dca37f34a38b5
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-14 13:54:22 +0200

    Edited index.js

--- cat-shelter-lab/server/index.js
+++ cat-shelter-lab/server/index.js
@@ -1,20 +1,28 @@
 const http = require("http");
-const hostname = '127.0.0.1';
+
+// Router
+const router = require("./router/router");
+const addBreedHandler = require('./handlers/addBreed');
+const homeHandler = require('./handlers/home');
+
+// Routes
+router.get('/', homeHandler.get);
+router.get('/cats/add-breed', addBreedHandler.get);
+
+
+const hostname = "127.0.0.1";
 const port = 3000;
+const server = http.createServer(requestHandler);
 
-// Handlers
-const handlers = require('./handlers')
+function requestHandler(req, res) {
+  const url = new URL(req.url, "http://localhost");
 
-const server = http.createServer((req, res) => {
-   
-    for (let handler of handlers) {
-        if(!handler(req, res)) {
-            break;
-        }
-    }
+  console.log(req.method, req.url);
 
-})
+  const handler = router.match(req.method, url.pathname);
+  handler(req, res);
+}
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
