9e632c6bc6ed5f4e7c52b2aa7258b74416cb6398
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-14 13:53:29 +0200

    Edited home.js

--- cat-shelter-lab/server/handlers/home.js
+++ cat-shelter-lab/server/handlers/home.js
@@ -1,29 +1,27 @@
-const url = require("url");
-const fs = require("fs");
+const fsp = require("fs/promises");
 const path = require("path");
 
-// Objects
-const cats = require("../data/cats");
-const breeds = require("../data/breeds");
+async function get(req, res) {
+  const filePath = path.normalize(path.join(__dirname, "../views/index.html"));
 
-module.exports = (req, res) => {
-  const pathname = url.parse(req.url).pathname;
-
-  if (pathname === "/" && req.method === "GET") {
-    let filePath = path.normalize(path.join(__dirname, "../views/index.html"));
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    fs.readFile(filePath, (error, data) => {
-      if (error) {
-        res.writeHead(404);
-        res.write("Whoops! File not found!");
-      } else {
-        res.write(data);
-      }
-      res.end();
+  try {
+    let data = await fsp.readFile(filePath);
+    res.writeHead(200, {
+      "Content-Type": "text/html",
     });
-  } else {
-    return true;
+    res.write(data);
+    res.end();
+  } catch {
+    res.writeHead(404, {
+      "Content-Type": "text/plain",
+    });
+    res.write("404 Not Found");
+    res.end();
   }
+}
+
+const homeHandler = {
+  get,
 };
+
+module.exports = homeHandler;
