5b4a41097685711d9789db91df28d79f730d2d11
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-15 13:43:41 +0200

    Implemented post request handler for breed

--- cat-shelter-lab/server/handlers/addBreed.js
+++ cat-shelter-lab/server/handlers/addBreed.js
@@ -23,8 +23,44 @@ async function get(req, res) {
   }
 }
 
+function post(req, res) {
+  // Parse the incoming data from the form
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err, field) => {
+    try {
+      if (err) {
+        throw err;
+      }
+
+      // Read the breeds.json file
+      const data = await fsp.readFile("./data/breeds.json");
+      let existing = JSON.parse(data);
+
+      // Modify the breeds.json file
+      if (!existing.includes(field.breed)) {
+        existing.push(field.breed);
+        let updated = JSON.stringify(existing);
+
+        // Update the breeds.json file
+        await fsp.writeFile("./data/breeds.json", updated, "utf-8");
+
+        // Redirect to the home page ('/') and end the response
+        res.writeHead(301, {
+          Location: "/",
+        });
+        res.end();
+      }
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write(error.message);
+      res.end();
+    }
+  });
+}
+
 const addBreedHandler = {
   get,
+  post,
 };
 
-module.exports = addBreedHandler
+module.exports = addBreedHandler;
