82e141b61a6c5968e31fb18d29312f0bcf3c95db
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-16 15:43:41 +0200

    Edited home.js

--- cat-shelter-lab/server/handlers/home.js
+++ cat-shelter-lab/server/handlers/home.js
@@ -12,14 +12,16 @@ async function get(req, res) {
       "Content-Type": "text/html",
     });
 
-    const modifiedData = data.toString().replace('{{allCats}}', allCatsTemplate)
+    const modifiedData = data
+      .toString()
+      .replace("{{allCats}}", allCatsTemplate);
     res.write(modifiedData);
     res.end();
-  } catch {
+  } catch (error) {
     res.writeHead(404, {
       "Content-Type": "text/plain",
     });
-    res.write("404 Not Found");
+    res.write(error.message);
     res.end();
   }
 }
