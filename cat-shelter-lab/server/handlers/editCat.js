cc91df15f29266f8a281087f6f339da49113e0dc
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-16 13:55:11 +0200

    Implemented post request handler for editCat

--- cat-shelter-lab/server/handlers/editCat.js
+++ cat-shelter-lab/server/handlers/editCat.js
@@ -1,8 +1,9 @@
+const formidable = require("formidable");
 const fsp = require("fs/promises");
 const path = require("path");
 
 const getAllCats = require("../utils/getData");
-const {allBreedsTemplate} = require('../utils/template')
+const { allBreedsTemplate } = require("../utils/template");
 
 async function get(req, res) {
   const filepath = path.normalize(
@@ -42,9 +43,49 @@ async function get(req, res) {
   }
 }
 
+function post(req, res) {
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, file) => {
+    try {
+      if (err) {
+        throw err;
+      }
+      const { cats, cat } = await getAllCats(req);
+      Object.assign(cat, fields);
 
-const editCatHandler = {
-    get
+      if (file.upload.originalFilename != "") {
+        cat.image = file.upload.originalFilename;
+
+        const oldPath = file.upload.filepath;
+        const newPath = path.normalize(
+          path.join(
+            __dirname,
+            "../content/images",
+            file.upload.originalFilename
+          )
+        );
+
+        await fsp.copyFile(oldPath, newPath);
+        await fsp.unlink(oldPath);
+      }
+
+      const updated = JSON.stringify(cats, null, 2);
+      await fsp.writeFile("./data/cats.json", updated, "utf-8");
+      res.writeHead(301, {
+        Location: "/",
+      });
+      res.end();
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write(error.message);
+      res.end();
+    }
+  });
 }
 
-module.exports = editCatHandler
+const editCatHandler = {
+  get,
+  post,
+};
+
+module.exports = editCatHandler;
