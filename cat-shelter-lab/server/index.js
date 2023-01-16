b3228ad27a3230ef535ab2673c47484bb3ce3789
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-16 10:15:57 +0200

    Added post request handler

--- cat-shelter-lab/server/index.js
+++ cat-shelter-lab/server/index.js
@@ -5,6 +5,7 @@ const router = require("./router/router");
 const addBreedHandler = require("./handlers/addBreed");
 const addCatHandler = require("./handlers/addCat");
 const homeHandler = require("./handlers/home");
+const catShelterHandler = require("./handlers/catShelter");
 
 // Routes
 router.get("/", homeHandler.get);
@@ -12,6 +13,8 @@ router.get("/cats/add-breed", addBreedHandler.get);
 router.post("/cats/add-breed", addBreedHandler.post);
 router.get("/cats/add-cat", addCatHandler.get);
 router.post("/cats/add-cat", addCatHandler.post);
+router.get("/cats-find-new-home", catShelterHandler.get);
+router.post("/cats-find-new-home", catShelterHandler.post);
 
 const hostname = "127.0.0.1";
 const port = 3000;
