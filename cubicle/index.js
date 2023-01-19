8800c5233aa8c032bfbde986789154b412949164
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-19 21:55:22 +0200

    Config view engine

--- cubicle/index.js
+++ cubicle/index.js
@@ -1,9 +1,15 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const config = require("./config/config")[env];
+const app = require("express")();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+require("./config/express")(app);
+require("./config/routes")(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.listen(
+  config.port,
+  console.log(`Listening on port ${config.port}! Now its up to you...`)
+);
