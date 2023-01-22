10a04e35796866d6fa575a9d3b5d7a1a02b4b8f5
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-22 11:15:29 +0200

    Edited router.js

--- cubicle/config/routes.js
+++ cubicle/config/routes.js
@@ -1,5 +1,18 @@
-// TODO: Require Controllers...
+const express = require('express')
+const Router = express.Router
+const cubeController = require("../controllers/cubeController");
 
+const router = Router()
 module.exports = (app) => {
-    // TODO...
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+  app.get("/about", (req, res) => {
+    res.render("about");
+  });
+
+  app.get("/create", cubeController.getCreateCube);
 };
+
+module.exports = router
