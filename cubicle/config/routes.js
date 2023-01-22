eae396a765685507560e27aca9957230e62c95b2
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-22 14:32:41 +0200

    Config routes.js

--- cubicle/config/routes.js
+++ cubicle/config/routes.js
@@ -1,11 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const cubeController = require("../controllers/cubeController");
 const homeController = require("../controllers/homeController");
 
+// Home routes
 router.get("/", homeController.getHomePage);
-
 router.get("/about", homeController.getAboutPage);
 
+// Cube routes
 router.get("/create", cubeController.getCreateCube);
+router.get("/create", cubeController.getCreateCube);
+router.post("/create", cubeController.postCreateCube);
 
-module.exports = router
+module.exports = router;
