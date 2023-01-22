a40cd8cc7a0406513cdc59441bdfefd3324f083f
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-22 11:56:45 +0200

    Config routes

--- cubicle/config/express.js
+++ cubicle/config/express.js
@@ -2,14 +2,12 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-
 module.exports = (app) => {
     // Setup the view engine
     app.engine("hbs", handlebars({
         extname: 'hbs'
     }));
     app.set("view engine", "hbs");
-
 
     //TODO: Setup the body parser
 
