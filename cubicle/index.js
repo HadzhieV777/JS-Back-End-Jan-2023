0dc1d61ab22d43fd0133eda68224f5c26f2e3fe6
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-22 11:55:51 +0200

    Added middlewares to handle routes

--- cubicle/index.js
+++ cubicle/index.js
@@ -1,11 +1,14 @@
 const env = process.env.NODE_ENV || "development";
-
 const config = require("./config/config")[env];
 const app = require("express")();
-
+const routes = require("./config/routes");
 require("./config/express")(app);
-require("./config/routes")(app);
 
+// Middlewares
+app.use("/", routes);
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
 app.listen(
   config.port,
