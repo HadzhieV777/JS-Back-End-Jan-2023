55a0f0d67e3d7bbd9d1cc21c87c0ec44a9692e5d
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-22 14:32:20 +0200

    Setup body parser middleware

--- cubicle/config/express.js
+++ cubicle/config/express.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 module.exports = (app) => {
-    // Setup the view engine
-    app.engine("hbs", handlebars({
-        extname: 'hbs'
-    }));
-    app.set("view engine", "hbs");
+  // Setup the view engine
+  app.engine("hbs", handlebars({
+      extname: "hbs",
+    })
+  );
+  app.set("view engine", "hbs");
 
-    //TODO: Setup the body parser
+  // Setup the body parser
+  app.use(express.urlencoded({ extended: false }));
 
-    // Setup the static files
-    app.use(express.static('static'))
+  // Setup the static files
+  app.use(express.static("static"));
 
+  // Setup routes   
+  app.use("/", routes);
 
+  // Handle not found   
+  app.use((req, res) => {
+    res.status(404).render("404");
+  });
 };
