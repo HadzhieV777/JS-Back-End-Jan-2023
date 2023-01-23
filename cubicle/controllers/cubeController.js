d68282aad61ac76048d604550ea9f563e07bac72
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-23 11:43:35 +0200

    Edited cubeController

--- cubicle/controllers/cubeController.js
+++ cubicle/controllers/cubeController.js
@@ -1,11 +1,32 @@
 const Cube = require("../models/Cube");
+const db = require("../config/database.json");
 
 exports.getCreateCube = (req, res) => {
   res.render("create");
 };
 
 exports.postCreateCube = (req, res) => {
-  let cube = new Cube(req.body);
-  Cube.save(cube)
+  let cube = new Cube(
+    req.body.name,
+    req.body.description,
+    req.body.imageUrl,
+    req.body.difficultyLevel
+  );
+  Cube.save(cube);
   res.redirect("/");
+};
+
+exports.getDetails = (req, res) => {
+  let cubeId = Number(req.params.cubeId);
+
+  if (!cubeId) {
+    return res.redirect("/404");
+  }
+
+  let cube = db.cubes.find((x) => x.id === cubeId);
+
+  if (!cube) {
+    return res.redirect("/404");
+  }
+  res.render("details", { cube });
 };
