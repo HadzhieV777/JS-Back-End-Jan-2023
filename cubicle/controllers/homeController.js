6a675370076d1b0de6979e4cd783c98bbb5cfcaa
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-23 11:43:55 +0200

    Added sort functionality

--- cubicle/controllers/homeController.js
+++ cubicle/controllers/homeController.js
@@ -1,8 +1,26 @@
+const db = require("../config/database.json");
+
 exports.getHomePage = (req, res) => {
-  res.render("index");
+  let cubes = db.cubes;
+  const { search, from, to } = req.query;
+
+  if (search) {
+    cubes = cubes.filter((cube) =>
+      cube.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  if (from) {
+    cubes = cubes.filter((cube) => cube.difficultyLevel >= from);
+  }
+
+  if (to) {
+    cubes = cubes.filter((cube) => cube.difficultyLevel <= to);
+  }
+
+  res.render("index", { cubes, search, from, to });
 };
 
 exports.getAboutPage = (req, res) => {
   res.render("about");
 };
-
