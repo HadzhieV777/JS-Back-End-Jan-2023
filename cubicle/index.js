5d791209c5f314d865aa72e43646cac772f915bb
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-29 11:42:59 +0200

    Setting up mongoose

--- cubicle/index.js
+++ cubicle/index.js
@@ -2,9 +2,15 @@ const env = process.env.NODE_ENV || "development";
 const config = require("./config/config")[env];
 const app = require("express")();
 
+const initDatabase = require("./config/databaseInit");
+
 require("./config/express")(app);
 
-app.listen(
-  config.port,
-  console.log(`Listening on port ${config.port}! Now its up to you...`)
-);
+initDatabase()
+  .then(() =>
+    app.listen(
+      config.port,
+      console.log(`Listening on port ${config.port}! Now its up to you...`)
+    )
+  )
+  .catch((err) => console.log(err));
