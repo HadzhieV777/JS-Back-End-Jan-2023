50ed173a832fa54f6e3d5dc98cbcbdff367c4843
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-30 10:02:16 +0200

    Edited cubeController

--- cubicle/controllers/cubeController.js
+++ cubicle/controllers/cubeController.js
@@ -26,7 +26,9 @@ exports.getDetails = async (req, res) => {
 
 exports.getAttachAccessory = async (req, res) => {
   const cube = await Cube.findById(req.params.cubeId).lean();
-  const accessories = await Accessory.find().lean();
+  const accessories = await Accessory.find({
+    _id: { $nin: cube.accessories }
+  }).lean();
 
   res.render("cube/attach", { cube, accessories });
 };
