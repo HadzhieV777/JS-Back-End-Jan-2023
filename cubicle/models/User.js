787caec2afa16c363c722400c4de49bbbea81c67
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-04 17:32:38 +0200

    Add user model

--- cubicle/models/User.js
+++ cubicle/models/User.js
@@ -1,1 +1,12 @@
 const { Schema, model, Types } = require("mongoose");
+
+const cubeSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: S,
+    required: true,
+  },
+});
