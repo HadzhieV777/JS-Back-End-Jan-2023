afea4def9831356f3505f3d5eeea25939e4a6f15
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-04 22:00:07 +0200

    Handle register

--- cubicle/managers/authManager.js
+++ cubicle/managers/authManager.js
@@ -2,5 +2,4 @@ const User = require("../models/User");
 
 exports.getByUsername = (username) => User.findOne({ username });
 
-exports.register = (username, plainPass) =>
-  User.create({ username, plainPass });
+exports.register = (username, password) => User.create({ username, password });
