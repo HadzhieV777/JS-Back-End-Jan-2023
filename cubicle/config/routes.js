d2a8aa60856e932b566eb3ba8791231256b574c9
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-05 15:11:18 +0200

    Added guards

--- cubicle/config/routes.js
+++ cubicle/config/routes.js
@@ -3,24 +3,27 @@ const cubeController = require("../controllers/cubeController");
 const homeController = require("../controllers/homeController");
 const accessoryController = require("../controllers/accessoryController");
 const authController = require("../controllers/authController");
+const { isAuthenticated } = require("../middlewares/authMiddleware");
 
 // Auth routes
-router.use('/auth', authController)
+router.use("/auth", authController);
 
 // Home routes
 router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
 
 // Cube routes
-router.get("/cubes/create", cubeController.getCreateCube);
+router.get("/cubes/create", isAuthenticated, cubeController.getCreateCube);
 router.post("/cubes/create", cubeController.postCreateCube);
 router.get("/cubes/:cubeId/details", cubeController.getDetails);
 router.get("/cubes/:cubeId/attach", cubeController.getAttachAccessory);
 router.post("/cubes/:cubeId/attach", cubeController.postAttachAccessory);
 router.get("/cubes/:cubeId/edit", cubeController.getEditCube);
+router.post("/cubes/:cubeId/edit", cubeController.postEditCube);
 router.get("/cubes/:cubeId/delete", cubeController.getDeleteCube);
+router.post("/cubes/:cubeId/delete", cubeController.postDeleteCube);
 
 // Accessory routes
-router.use('/accessories', accessoryController)
+router.use("/accessories", accessoryController);
 
 module.exports = router;
