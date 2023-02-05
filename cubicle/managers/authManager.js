ba41dccc762abf2331bfa98fcd2cbb7ebd6d445e
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-05 11:38:36 +0200

    Handle login

--- cubicle/managers/authManager.js
+++ cubicle/managers/authManager.js
@@ -3,3 +3,16 @@ const User = require("../models/User");
 exports.getByUsername = (username) => User.findOne({ username });
 
 exports.register = (username, password) => User.create({ username, password });
+
+exports.login = async (username, password) => {
+  const user = await this.getByUsername(username);
+
+  const isValid = await user.validatePassword(password);
+
+  // (!(!user && !isValid))
+  if (!user || !isValid) {
+    throw "Invalid username or password!";
+  }
+
+  return user;
+};
