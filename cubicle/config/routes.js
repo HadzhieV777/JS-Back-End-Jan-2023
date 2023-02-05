c5e747ffd540a22f44fd9f1073bf88273d2e0a8b
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-05 11:37:56 +0200

    Added auth routes

--- cubicle/config/routes.js
+++ cubicle/config/routes.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const cubeController = require("../controllers/cubeController");
 const homeController = require("../controllers/homeController");
 const accessoryController = require("../controllers/accessoryController");
+const authController = require("../controllers/authController");
+
+// Auth routes
+router.use('/auth', authController)
 
 // Home routes
 router.get("/", homeController.getHomePage);
@@ -13,6 +17,8 @@ router.post("/cubes/create", cubeController.postCreateCube);
 router.get("/cubes/:cubeId/details", cubeController.getDetails);
 router.get("/cubes/:cubeId/attach", cubeController.getAttachAccessory);
 router.post("/cubes/:cubeId/attach", cubeController.postAttachAccessory);
+router.get("/cubes/:cubeId/edit", cubeController.getEditCube);
+router.get("/cubes/:cubeId/delete", cubeController.getDeleteCube);
 
 // Accessory routes
 router.use('/accessories', accessoryController)
