ba10c1d4ab53cbb882cba064cd5f5a8ead73fe23
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-06 19:58:30 +0200

    Edited authMiddleware

--- cubicle/middlewares/authMiddleware.js
+++ cubicle/middlewares/authMiddleware.js
@@ -9,10 +9,23 @@ exports.auth = async (req, res, next) => {
       const decodedToken = await jwt.verify(token, config.development.SECRET);
 
       req.user = decodedToken;
+      req.isAuthenticated = true;
+      res.locals.username = decodedToken.username;
+      res.locals.isAuthenticated = true;
     } catch (err) {
-      console.log(err);
+      console.log(err.message);
       res.clearCookie("auth");
       res.redirect("/404");
     }
   }
+
+  next();
+};
+
+exports.isAuthenticated = (req, res, next) => {
+  if (!req.isAuthenticated) {
+    return res.redirect("/auth/login");
+  }
+
+  next();
 };
