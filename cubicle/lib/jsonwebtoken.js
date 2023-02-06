f8fa38599bc504c2d9417f43bef141af692552f5
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-06 19:57:32 +0200

    Config jsonwebtoken

---
+++ cubicle/lib/jsonwebtoken.js
@@ -0,0 +1,8 @@
+const util = require("util");
+const jwtCallback = require("jsonwebtoken");
+const jwt = {
+  sign: util.promisify(jwtCallback.sign),
+  verify: util.promisify(jwtCallback.verify),
+};
+
+module.exports = jwt;
