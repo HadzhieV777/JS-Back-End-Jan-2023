c9a3d2fcda0e6c202452875731e9d2b77fdc837c
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-11 16:37:28 +0200

    Refactoring authController

--- 11.Exam-Preparation/BookingUni/controllers/authController.js
+++ 11.Exam-Preparation/BookingUni/controllers/authController.js
@@ -1,10 +1,10 @@
+const validator = require("validator");
 const { register, login } = require("../services/userService");
 const { parseError } = require("../utils/parser");
 
 const authController = require("express").Router();
 
 authController.get("/register", (req, res) => {
-  // TODO replace with actual view by assignment
   res.render("auth/register", {
     title: "Register Page",
   });
@@ -12,28 +12,33 @@ authController.get("/register", (req, res) => {
 
 authController.post("/register", async (req, res) => {
   try {
+    if (!validator.isEmail(req.body.email)) {
+      throw new Error("Invalid Email!");
+    }
     if (req.body.username == "" || req.body.password == "") {
       throw new Error("All fields are required!");
     }
 
-    // TODO change rePass name if needed
+    if (req.body.password.length < 5) {
+      throw new Error("Password must be at least 5 characters long!");
+    }
+
     if (req.body.password != req.body.rePass) {
       throw new Error("Passwords not match!");
     }
 
-    const token = await register(req.body.username, req.body.password);
+    const token = await register(req.body.email, req.body.username, req.body.password);
 
-    // TODO check assignment to see if register creates session
     res.cookie("auth", token);
-    res.redirect("/"); // TODO replace with redirect by assignment
+    res.redirect("/");
   } catch (error) {
     const errors = parseError(error);
 
-    // TODO add error display to actual template from assignment
     res.render("auth/register", {
       title: "Register Page",
       errors,
       body: {
+        email: req.body.email,
         username: req.body.username,
       },
     });
@@ -41,7 +46,6 @@ authController.post("/register", async (req, res) => {
 });
 
 authController.get("/login", (req, res) => {
-  // TODO replace with actual view by assignment
   res.render("auth/login", {
     title: "Login Page",
   });
@@ -49,29 +53,29 @@ authController.get("/login", (req, res) => {
 
 authController.post("/login", async (req, res) => {
   try {
-    if (req.body.username == "" || req.body.password == "") {
+    if (req.body.email == "" || req.body.password == "") {
       throw new Error("All fields are required!");
     }
 
-    const token = await login(req.body.username, req.body.password);
+    const token = await login(req.body.email, req.body.password);
 
     res.cookie("auth", token);
-    res.redirect("/"); // TODO replace with redirect by assignment
+    res.redirect("/"); 
   } catch (error) {
     const errors = parseError(error);
     res.render("auth/login", {
       title: "Login Page",
       errors,
       body: {
-        username: req.body.username,
+        email: req.body.email,
       },
     });
   }
 });
 
-authController.get('/logout', (req, res) => {
-  res.clearCookie('auth')
+authController.get("/logout", (req, res) => {
+  res.clearCookie("auth");
   res.redirect("/");
-})
+});
 
 module.exports = authController;
