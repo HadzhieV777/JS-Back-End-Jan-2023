2ae8e97c5bc8398a9f899a8185ab411a32988c95
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-11 16:37:44 +0200

    Added route guards

--- 11.Exam-Preparation/BookingUni/config/routes.js
+++ 11.Exam-Preparation/BookingUni/config/routes.js
@@ -1,12 +1,14 @@
 const authController = require("../controllers/authController");
 const homeController = require("../controllers/homeController");
+const hotelController = require("../controllers/hotelController");
+const profileController = require("../controllers/profileController");
+const { isUser } = require("../middlewares/guards");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
-
-
-
+  app.use("/hotel", isUser(), hotelController); 
+  app.use("/profile", profileController);
   // Handle not found
   // app.use((req, res) => {
   //   res.status(404).render("404");
