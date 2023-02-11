2bca6e010b464c69bf956cc64ded0893a72c6c9d
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-11 16:37:13 +0200

    Handle catalog view

--- 11.Exam-Preparation/BookingUni/controllers/homeController.js
+++ 11.Exam-Preparation/BookingUni/controllers/homeController.js
@@ -1,10 +1,12 @@
+const { getAll } = require("../services/hotelService");
+
 const homeController = require("express").Router();
 
-// TODO replace with real controller by assignment
-homeController.get("/", (req, res) => {
+homeController.get("/", async (req, res) => {
+  const hotels = await getAll()
   res.render("home", {
     title: "Home Page",
-    user: req.user,
+    hotels
   });
 });
 
