1c93f8840ab768ca039e5ad2a65bb03f858581fd
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-11 16:36:31 +0200

    Added custom validation

--- 11.Exam-Preparation/BookingUni/models/User.js
+++ 11.Exam-Preparation/BookingUni/models/User.js
@@ -1,18 +1,31 @@
 const { Schema, model } = require("mongoose");
 
-// TODO add User properties and validation according to assignment
 const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
   username: {
     type: String,
     required: true,
     unique: true,
-    minLength: [3, "Username must be at least 3 characters long!"],
+    match: [
+      /^[a-zA-Z0-9]+$/i,
+      "Username must contain only english letters and numbers!",
+    ],
   },
   hashedPassword: { type: String, required: true },
 });
 
 userSchema.index(
   { username: 1 },
+  {
+    collation: {
+      locale: "en",
+      strength: 2,
+    },
+  }
+);
+
+userSchema.index(
+  { email: 1 },
   {
     collation: {
       locale: "en",
