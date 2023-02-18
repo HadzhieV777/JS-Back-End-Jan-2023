c893ba59f20d7576160c7220a46e5d92d72a9644
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-18 22:23:56 +0200

    Added config dir

---
+++ 11.Exam-Preparation/BookTalk/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const CONNECTION_STRING = "mongodb://127.0.0.1:27017/bookTalkDB";
+
+module.exports = async (app) => {
+  try {
+    mongoose.set("strictQuery", false);
+    
+    await mongoose.connect(CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Database connected...")
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
