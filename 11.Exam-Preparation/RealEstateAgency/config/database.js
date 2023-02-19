18cb1b6092951a88f97162839df240903927ef64
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-19 10:57:12 +0200

    Config db

--- 11.Exam-Preparation/RealEstateAgency/config/database.js
+++ 11.Exam-Preparation/RealEstateAgency/config/database.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
-// TODO change database name according to assignment
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017/scaffoldDB";
+const CONNECTION_STRING = "mongodb://127.0.0.1:27017/realEstatesDB";
 
 module.exports = async (app) => {
   try {
     mongoose.set("strictQuery", false);
-    
+
     await mongoose.connect(CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log("Database connected...")
+    console.log("Database connected...");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
