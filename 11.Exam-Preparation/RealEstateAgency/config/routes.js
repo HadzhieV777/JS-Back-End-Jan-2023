b7a495492e0d5518295ccdda2279a488cb273610
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-19 10:57:36 +0200

    Added routes guards

--- 11.Exam-Preparation/RealEstateAgency/config/routes.js
+++ 11.Exam-Preparation/RealEstateAgency/config/routes.js
@@ -1,11 +1,12 @@
 const authController = require("../controllers/authController");
 const homeController = require("../controllers/homeController");
 const housingController = require("../controllers/housingController");
+const { isUser } = require("../middlewares/guards");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
-  app.use("/housing", housingController);
+  app.use("/housing", isUser(), housingController);
 
   app.use("*", (req, res) => {
     res.render("404");
