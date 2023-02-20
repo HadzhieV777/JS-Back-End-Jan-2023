7ebda728727204246a5e94c9ef0b8c904edd68e1
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-20 09:14:22 +0200

    Config routes

--- 11.Exam-Preparation/BookTalk/config/routes.js
+++ 11.Exam-Preparation/BookTalk/config/routes.js
@@ -1,10 +1,12 @@
 const authController = require("../controllers/authController");
 const homeController = require("../controllers/homeController");
+const bookController = require("../controllers/bookController");
+const { isUser } = require("../middlewares/guards");
 
 module.exports = (app) => {
   app.use("/", homeController);
   app.use("/auth", authController);
-
+  app.use("/books", isUser(), bookController);
 
   app.use("*", (req, res) => {
     res.render("404");
