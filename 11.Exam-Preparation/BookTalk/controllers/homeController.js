b82289ac4dc54266860db2a20ab1ef32017fb49c
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-02-20 09:14:59 +0200

    Edited homeController

--- 11.Exam-Preparation/BookTalk/controllers/homeController.js
+++ 11.Exam-Preparation/BookTalk/controllers/homeController.js
@@ -1,5 +1,5 @@
 const homeController = require("express").Router();
-
+const { getAll, getById } = require("../services/bookService");
 
 homeController.get("/", (req, res) => {
   res.render("home", {
@@ -7,9 +7,34 @@ homeController.get("/", (req, res) => {
   });
 });
 
-homeController.get("/catalog", (req, res) => {
+homeController.get("/catalog", async (req, res) => {
+  const books = await getAll();
   res.render("catalog", {
     title: "Catalog",
+    books,
+  });
+});
+
+homeController.get("/details/:id", async (req, res) => {
+  const book = await getById(req.params.id);
+
+  if (req.user) {
+    book.hasUser = true;
+
+    if (req.user) {
+      if (book.owner == req.user._id) {
+        book.isOwner = true;
+      } else if (
+        book.wishList.map((u) => u.toString()).includes(req.user._id.toString())
+      ) {
+        book.isWished = true;
+      }
+    }
+  }
+
+  res.render("books/details", {
+    title: "Book Details",
+    book,
   });
 });
 
